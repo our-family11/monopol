39c4bc3c2e6b5822b8bf06ab09b0291e44aa8e4b
Tamar Kelerman <tamar.klirs@amdocs.com>
2025-07-22 18:11:32 +0300

    init commit - add project structure

---
+++ src/components/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-root">
+      <h1 className="dashboard-title">Monopoly Game Dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
